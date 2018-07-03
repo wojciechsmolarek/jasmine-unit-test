195648f3764318d66827ec21df86d387ac760d51
Wojciech Smolarek <kontakt@wojciechsmolarek.pl>
2018-07-03 11:14:12 +0200

    expanded rectArea function

--- app/rectArea.js
+++ app/rectArea.js
@@ -1,5 +1,30 @@
-function rectArea() {
-    return 6*6;
+function Rectangle(a,b) {
+    this.setA(a);
+    this.setB(b);
 }
 
-module.exports = rectArea;
+Rectangle.prototype.countArea = function () { 
+    return (this.a*this.b);
+ }
+
+ Rectangle.prototype.setA = function (valueA) { 
+    var a = parseFloat(valueA);
+
+    if (a>0) {
+        this.a = a;
+        return true;
+    }
+    return false;
+  }
+
+  Rectangle.prototype.setB = function (valueB) { 
+    var b = parseFloat(valueB);
+
+    if (b>0) {
+        this.b = b;
+        return true;
+    }
+    return false;
+  }
+
+module.exports = Rectangle;
--- spec/rectAreaSpec.js
+++ spec/rectAreaSpec.js
@@ -1,7 +1,20 @@
-var rectArea = require('../app/rectArea.js');
+var Rectangle = require('../app/rectArea.js');
 
-describe('rectArea', () => {
-    it('should return 27', () => {
-        expect(rectArea()).toBe(36);
+describe('Rectangle', () => {
+    var rect = new Rectangle(5,5);
+    var result;
+
+    describe('when numbers are equal, lower than 0', () => {
+        it('should return false', () => {
+            rect.setA(0);
+            rect.setB(0);
+
+            expect(result).toBeFalsy();
+        });
+    })
+
+    xit('should return 25', () => {
+        expect(rect.countArea()).toBe(25);        
     });
+
 })
